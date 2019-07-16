5778d067fe381b561f3116b90ae3a18fb30e108d
Mick van Duijn <mick.van.duijn@new10.com>
2019-07-16 07:52:28 +0200

    feat(target): added target configuration

--- index.js
+++ index.js
@@ -7,7 +7,10 @@
  * @license MIT
  */
 
-const id = Date.now().toString() + '-' + ('0000' + Math.floor(Math.random()*1000).toString()).substr(-4)
+const id =
+  Date.now().toString() +
+  '-' +
+  ('0000' + Math.floor(Math.random() * 1000).toString()).substr(-4)
 
 let warm = false
 let lastAccess = null
@@ -16,39 +19,32 @@ const funcName = process.env.AWS_LAMBDA_FUNCTION_NAME
 
 const delay = ms => new Promise(res => setTimeout(res, ms))
 
-module.exports = (event,cfg = {}) => {
-
-  let config = Object.assign({}, {
-    flag: 'warmer', // default test flag
-    concurrency: 'concurrency', // default concurrency field
-    target: 'target', // default target field
-    test: 'test', // default test flag
-    log: true, // default logging to true
-    correlationId: id, // default the correlationId
-    delay: 75, // default the delay to 75ms
-  },cfg)
-
+const handleEvent = (event, config) => {
   // If the event is a warmer ping
   if (event && event[config.flag]) {
+    let concurrency =
+      event[config.concurrency] &&
+      !isNaN(event[config.concurrency]) &&
+      event[config.concurrency] > 1
+        ? event[config.concurrency]
+        : 1
 
-    let concurrency = event[config.concurrency]
-      && !isNaN(event[config.concurrency])
-      && event[config.concurrency] > 1
-      ? event[config.concurrency] : 1
-    
     // Default target to funcName
     let target = event[config.target] || funcName
 
-    let invokeCount = event['__WARMER_INVOCATION__']
-      && !isNaN(event['__WARMER_INVOCATION__'])
-      ? event['__WARMER_INVOCATION__'] : 1
+    let invokeCount =
+      event['__WARMER_INVOCATION__'] && !isNaN(event['__WARMER_INVOCATION__'])
+        ? event['__WARMER_INVOCATION__']
+        : 1
 
-    let invokeTotal = event['__WARMER_CONCURRENCY__']
-      && !isNaN(event['__WARMER_CONCURRENCY__'])
-      ? event['__WARMER_CONCURRENCY__'] : concurrency
+    let invokeTotal =
+      event['__WARMER_CONCURRENCY__'] && !isNaN(event['__WARMER_CONCURRENCY__'])
+        ? event['__WARMER_CONCURRENCY__']
+        : concurrency
 
     let correlationId = event['__WARMER_CORRELATIONID__']
-      ? event['__WARMER_CORRELATIONID__'] : config.correlationId
+      ? event['__WARMER_CORRELATIONID__']
+      : config.correlationId
 
     // Create log record
     let log = {
@@ -60,7 +56,10 @@ module.exports = (event,cfg = {}) => {
       concurrency: invokeTotal,
       warm,
       lastAccessed: lastAccess,
-      lastAccessedSeconds: lastAccess === null ? null : ((Date.now()-lastAccess)/1000).toFixed(1)
+      lastAccessedSeconds:
+        lastAccess === null
+          ? null
+          : ((Date.now() - lastAccess) / 1000).toFixed(1)
     }
 
     // Log it
@@ -75,7 +74,6 @@ module.exports = (event,cfg = {}) => {
 
     // Fan out if concurrency is set higher than 1
     if ((concurrency > 1 || isDifferentTarget) && !event[config.test]) {
-
       // init Lambda service
       let lambda = require('./lib/lambda-service')
 
@@ -83,30 +81,28 @@ module.exports = (event,cfg = {}) => {
       let invocations = []
 
       // loop through concurrency count
-      for (let i=(isDifferentTarget ? 1 : 2); i <= concurrency; i++) {
-
+      for (let i = isDifferentTarget ? 1 : 2; i <= concurrency; i++) {
         // Set the params and wait for the final function to finish
         let params = {
           FunctionName: target,
           InvocationType: i === concurrency ? 'RequestResponse' : 'Event',
           LogType: 'None',
-          Payload: new Buffer(JSON.stringify({
-            [config.flag]: true, // send warmer flag
-            '__WARMER_INVOCATION__': i, // send invocation number
-            '__WARMER_CONCURRENCY__': concurrency, // send total concurrency
-            '__WARMER_CORRELATIONID__': correlationId // send correlation id
-          }))
+          Payload: new Buffer(
+            JSON.stringify({
+              [config.flag]: true, // send warmer flag
+              __WARMER_INVOCATION__: i, // send invocation number
+              __WARMER_CONCURRENCY__: concurrency, // send total concurrency
+              __WARMER_CORRELATIONID__: correlationId // send correlation id
+            })
+          )
         }
 
         // Add promise to invocations array
         invocations.push(lambda.invoke(params).promise())
-
       } // end for
 
       // Invoke concurrent functions
-      return Promise.all(invocations)
-        .then((res) => true)
-
+      return Promise.all(invocations).then(() => true)
     } else if (invokeCount > 1) {
       return delay(config.delay).then(() => true)
     }
@@ -117,5 +113,33 @@ module.exports = (event,cfg = {}) => {
     lastAccess = Date.now()
     return Promise.resolve(false)
   }
+}
 
+module.exports = (event, cfg = {}) => {
+  let config = Object.assign(
+    {},
+    {
+      flag: 'warmer', // default test flag
+      concurrency: 'concurrency', // default concurrency field
+      target: 'target', // default target field
+      test: 'test', // default test flag
+      log: true, // default logging to true
+      correlationId: id, // default the correlationId
+      delay: 75 // default the delay to 75ms
+    },
+    cfg
+  )
+
+  if (Array.isArray(event)) {
+    let i = 0
+    const handleNext = () => {
+      if (i < event.length) {
+        return handleEvent(event[i++], config).then(handleNext)
+      }
+      return Promise.resolve(true)
+    }
+    return handleNext()
+  } else {
+    return handleEvent(event, config)
+  }
 } // end module
