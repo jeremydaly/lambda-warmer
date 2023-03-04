528d9c0b4dff18e9dbe466f3bbe1789355813893
Naor Peled <thehecticbyte@gmail.com>
2023-03-04 16:33:05 +0100

    test(lambda-service): add coverage to invoke func

---
+++ test/lambda-service.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect // assertion library
+const sinon = require('sinon') // Require Sinon.js library
+
+const lambda = require('../lib/lambda-service') // Init Lambda Service
+
+let stub // init stub
+
+describe('Lambda service', () => {
+  beforeEach(function () {
+    // Stub invoke
+    stub = sinon.stub(lambda, 'invoke')
+  })
+
+  afterEach(function () {
+    stub.restore()
+  })
+
+  describe('invoke', () => {
+    it('should invoke a lambda function with given params', () => {
+      const params = {
+        FunctionName: 'test-function',
+        InvocationType: 'RequestResponse',
+      }
+
+      lambda.invoke(params)
+
+      expect(stub.calledOnce).to.be.true
+      expect(stub.calledWith(params)).to.be.true
+    })
+  })
+})
